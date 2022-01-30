4385dadc07990dd33e5413116d4e5d0661e60016
8242009 <81540565+8242009@users.noreply.github.com>
2022-01-30 09:37:28 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,23 @@ function preload(){
 	song=loadSound("music.mp3");
 	song_1=loadSound("music2.mp3");
 }
+
+leftWristX=0;
+leftWristX=0;
+rightWristX=0;
+rightWristY=0
+
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+
+    }
+}
